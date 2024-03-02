2d682ed6d59e8c70f1ca773ac1ef45339cc74765
jamez7 <kulikowskijacob@gmail.com>
2024-03-02 10:01:15 +0100

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] values = new int[2]
+
+
+
+        System.out.println(int[0]);
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        int y;
+
+        x = 10;
+        y = 5;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
